85c55439038f0e913157ba829a64b21b8f35b2e2
Djothi Carpentier <d.carpentier@criteo.com>
2021-02-02 16:36:47 +0100

    Support pip>=10

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 import uuid
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 __author__ = 'David Barroso <dbarrosop@dravetech.com>'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
+try:
+    reqs = [str(ir.req) for ir in install_reqs]
+except:
+    reqs = [str(ir.requirement) for ir in install_reqs]
 
 
 setup(
